9ab9f9cc0930216481966d10c881c9b471ee4b10
HamInGyu <hfire1207@gmail.com>
2021-05-17 15:32:07 +0900

    feat: week4 inClass study

---
+++ week4/firstproject/firstproject/urls.py
@@ -0,0 +1,34 @@
+"""firstproject URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from firstapp import views as first #view.firstapp.~~ 이렇게 들어갈 거를 first.~~ 이렇게 편하게
+from wordcount import views as wc
+
+#'' -> 최초의 페이지
+#두번째 인자 -> 연결할 view
+#세번째 인자 -> 편의를 위해 이렇게 부르겠다~
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', first.welcome, name="welcome"),
+    path('hello/', first.hello, name="hello"),
+
+    path('wc/', wc.home, name="wc"),
+    path('wc/result/', wc.result, name="result"),
+
+
+]
